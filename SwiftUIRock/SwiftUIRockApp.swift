15dfb9b8395878c5fc4c7e66c5b46f0ab62e6c34
Isaac Kim <myong328@gmail.com>
2022-04-05 12:19:05 -0700

    Initial Commit

---
+++ SwiftUIRock/SwiftUIRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRockApp.swift
+//  SwiftUIRock
+//
+//  Created by Isaac Kim on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
